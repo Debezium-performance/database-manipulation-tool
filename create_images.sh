f79480743b86be76acd47eb4e6a1eadfbb8e1e8d
Jiri Novotny <jnovotny@redhat.com>
2023-05-29 13:12:52 +0200

    Set whole image tag to be used as parameter

--- create_images.sh
+++ create_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./mvnw package -PnativeTest -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dskip.tests
-TAG="quay.io/tealc/debezium-database-manipulation-tool:$1"
+TAG="$1"
 docker build -f src/main/docker/Dockerfile.native -t ${TAG}-test .
 ./mvnw package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dskip.tests
 docker build -f src/main/docker/Dockerfile.native -t ${TAG} .

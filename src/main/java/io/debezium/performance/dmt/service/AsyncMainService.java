2c04596fc02e95d7a8e91ec4601c1299845b4164
Jiri Novotny <jnovotny@redhat.com>
2023-10-19 14:17:33 +0200

    Add Mongo bulk sized load generation and execution

--- src/main/java/io/debezium/performance/dmt/service/AsyncMainService.java
+++ src/main/java/io/debezium/performance/dmt/service/AsyncMainService.java
@@ -1,10 +1,14 @@
 package io.debezium.performance.dmt.service;
 
+import com.mongodb.client.model.WriteModel;
 import io.debezium.performance.dmt.async.ExecutorPool;
+import io.debezium.performance.dmt.dao.MongoDao;
 import io.debezium.performance.dmt.generator.Generator;
 import io.debezium.performance.dmt.model.DatabaseEntry;
+import io.debezium.performance.dmt.queryCreator.MongoBsonCreator;
 import io.debezium.performance.dmt.queryCreator.MysqlQueryCreator;
 import io.quarkus.runtime.Startup;
+import org.bson.Document;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -20,8 +24,11 @@ public class AsyncMainService extends MainService {
     ExecutorPool executorPool;
     @Inject
     MysqlQueryCreator mysqlQueryCreator;
-    public long[] generateLoad(int count, int maxRows) {
-        List<String> statements = generateQueries(count, maxRows);
+    @Inject
+    MongoBsonCreator mongoBsonCreator;
+
+    public long[] createAndExecuteLoad(int count, int maxRows) {
+        List<String> statements = generateAviationSqlQueries(count, maxRows);
         executorPool.setCountDownLatch(statements.size());
         long start = System.currentTimeMillis();
         for (String statement: statements) {
@@ -30,8 +37,8 @@ public class AsyncMainService extends MainService {
         return waitForLastTask(start);
     }
 
-    public long[] generateBatchLoad(int count, int maxRows) {
-        List<String> queries = generateQueries(count,maxRows);
+    public long[] createAndExecuteBatchLoad(int count, int maxRows) {
+        List<String> queries = generateAviationSqlQueries(count, maxRows);
         int poolSize = executorPool.getPoolSize();
         int batchSize = queries.size() / poolSize;
         List<List<String>> batches = new ArrayList<>();
@@ -46,6 +53,33 @@ public class AsyncMainService extends MainService {
         return waitForLastTask(start);
     }
 
+    public long[] createAndExecuteBatchLoadSecond(int count, int maxRows) {
+        List<String> queries = generateAviationSqlQueries(count, maxRows);
+        int poolSize = executorPool.getPoolSize();
+        int batchSize = queries.size() / poolSize;
+        List<List<String>> batches = new ArrayList<>();
+        for (int i = 0; i < queries.size(); i += batchSize) {
+            batches.add(queries.subList(i, Math.min(i + batchSize, queries.size())));
+        }
+        executorPool.setCountDownLatch(batches.size());
+        long start = System.currentTimeMillis();
+        for (List<String> batch: batches) {
+            executorPool.executeBatch(batch);
+            executorPool.executeFunction(dao -> dao.executeBatchStatement(batch));
+        }
+        return waitForLastTask(start);
+    }
+
+    public long createAndExecuteSizedMongoLoad(int count, int maxRows, int messageSize) {
+        String collection = generator.generateByteBatch(1,1,1).get(0).getDatabaseTableMetadata().getName();
+        List<WriteModel<Document>> bulkOperations = generateSizedMongoBulk(count, maxRows, messageSize);
+        MongoDao mongo = daoManager.getMongoDao();
+        long start = System.currentTimeMillis();
+        mongo.bulkWrite(bulkOperations, collection);
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+
     private long[] waitForLastTask(long start) {
         long lastThreadExecuted = System.currentTimeMillis() - start;
         long lastThreadFinished;
@@ -58,8 +92,8 @@ public class AsyncMainService extends MainService {
         return new long[] {lastThreadExecuted, lastThreadFinished};
     }
 
-    private List<String> generateQueries(int count, int maxRows) {
-        List<DatabaseEntry> entries = generator.generateBatch(count, maxRows);
+    private List<String> generateAviationSqlQueries(int count, int maxRows) {
+        List<DatabaseEntry> entries = generator.generateAviationBatch(count, maxRows);
         createTable(entries.get(0));
         List<String> queries = new ArrayList<>();
         for (DatabaseEntry entry: entries) {
@@ -70,6 +104,15 @@ public class AsyncMainService extends MainService {
             }
         }
         return queries;
+    }
+
+    private List<WriteModel<Document>> generateSizedMongoBulk(int count, int maxRows, int messageSize) {
+        List<DatabaseEntry> entries = generator.generateByteBatch(count, maxRows, messageSize);
+        createTable(entries.get(0));
+        for (DatabaseEntry entry: entries) {
+            database.upsertEntry(entry);
+        }
+        return mongoBsonCreator.bulkUpdateBson(entries);
     }
 
 }
